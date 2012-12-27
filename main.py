e8c92b1397a0055cfc99898fa690e3ae877714a0
Brian Dorsey <briandorsey@google.com>
2012-12-26 16:01:10 -0800

    annotate instance data and display annotations

--- main.py
+++ main.py
@@ -15,16 +15,21 @@
 # limitations under the License.
 #
 import os
+import datetime
 from pprint import pformat
 
-import webapp2
 import jinja2
+import webapp2
 from apiclient.discovery import build
 from oauth2client.appengine import OAuth2Decorator
 from oauth2client.appengine import OAuth2DecoratorFromClientSecrets
 
 SAMPLE_NAME = 'Instance timeout helper'
+# Be careful, this application will delete instances unless they're tagged
+# with one of the SAFE_TAGS below.
 GCE_PROJECT_ID = 'briandpe-api'
+TIMEOUT = 60  # minutes
+SAFE_TAGS = "production safe".split()
 
 decorator = OAuth2DecoratorFromClientSecrets(
     os.path.join(os.path.dirname(__file__), 'client_secrets.json'),
@@ -35,12 +40,33 @@ jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader('templates'))
 
 
+def annotate_instances(instances):
+    for instance in instances:
+        # set _excluded
+        excluded = False
+        for tag in instance.get('tags', []):
+            if tag in SAFE_TAGS:
+                excluded = True
+                break
+        instance['_excluded'] = excluded
+
+        # set _timeout_expired (never True for _excluded instances)
+        creation = parse_iso8601tz(instance['creationTimestamp'])
+        now = datetime.datetime.now()
+        delta = now - creation
+        if delta.seconds > TIMEOUT * 60 and not instance['_excluded']:
+            instance['_timeout_expired'] = True
+        else:
+            instance['_timeout_expired'] = False
+
+
 class MainHandler(webapp2.RequestHandler):
     @decorator.oauth_required
     def get(self):
         list_api = compute.instances().list(project=GCE_PROJECT_ID)
         result = list_api.execute(decorator.http())
         instances = result['items']
+        annotate_instances(instances)
 
         data = {}
         data['title'] = SAMPLE_NAME
@@ -54,3 +80,26 @@ app = webapp2.WSGIApplication([
     ('/', MainHandler),
     (decorator.callback_path, decorator.callback_handler()),
 ], debug=True)
+
+
+# ------------------------------------------------
+# helpers
+def parse_iso8601tz(date_string):
+    """return a datetime object for strings in ISO 8601 format.
+
+    This function only reliably parses strings in exactly this format:
+    '2012-12-26T13:31:47.823-08:00'
+
+    sadly, datetime.strptime's %z format is unavailable on many platforms.
+    """
+
+    dt = datetime.datetime.strptime(date_string[:-6],
+                                    '%Y-%m-%dT%H:%M:%S.%f')
+
+    # parse the timezone offset separately
+    delta = datetime.timedelta(minutes=int(date_string[-2:]),
+                               hours=int(date_string[-5:-3]))
+    if date_string[-6:-5] == u'-':
+        delta = delta * -1
+    dt = dt - delta
+    return dt
